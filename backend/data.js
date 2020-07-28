4e02e88f394e76d500813c6b6d565b5dc6f44f5a
Rahan Bouess <wangleibniz01@gmail.com>
2020-07-29 01:15:38 +0800

    first commit

---
+++ backend/data.js
@@ -0,0 +1,34 @@
+export default {
+    products:[ {
+        _id:'1',
+            name:'Slim shirt',
+            category:'Shirts',
+            image:'/images/1.jpg',
+            price:60,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10
+        },
+
+        {   _id:'2',
+            name:'Best Pants',
+            category:'Shirts',
+            image:'/images/2.jpg',
+            price:57,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:5
+        },
+
+        {   _id:'3',
+            name:'Fit shirt',
+            category:'Shirts',
+            image:'/images/3.jpg',
+            price:50,
+            brand:'Nike',
+            rating:3.5,
+            numReviews:10
+        }
+
+    ]
+}
---
+++ frontend/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter ,Route,Link} from 'react-router-dom'
+import ProductScreen from './Screens/ProductScreen'
+import HomeScreen from './Screens/HomeScreen'
+function App() {
+
+    function openMenu(){
+      document.querySelector(".sidebar").classList.add("open")
+    }
+    function closeMenu(){
+        document.querySelector(".sidebar").classList.remove("open")
+    }
+  return (
+    <BrowserRouter>
+    <div className="grid-container">
+        <header className="header">
+            <div className="brand">
+                <button onClick={openMenu}>
+                    &#9776;
+                </button>
+                <Link to='/'> amazona</Link></div>
+            <div className="header-links"><a href="cart.html">Cart</a><a href="signIn">Sign In</a></div>
+        </header>
+        <aside className="sidebar">
+            <h3>Shopping Categories</h3>
+            <button className="sidebar-close-button" onClick={closeMenu}>X</button>
+            <ul>
+                <li><a href="#">Pants</a></li>
+                <li><a href="#">Shirt</a></li>
+            </ul>
+        </aside>
+        <main className="main">
+          <div className="content">
+            <Route path="/product/:id" component={ProductScreen}/>
+            <Route path="/" exact={true} component={HomeScreen}/>
+           </div>
+        </main>
+
+        <footer className="footer">
+            all right reserved
+        </footer>
+    </div>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
