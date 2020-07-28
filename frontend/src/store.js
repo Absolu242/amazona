78c365374c0f5795cae4d03ba4bc540992b2fb41
Rahan Bouess <wangleibniz01@gmail.com>
2020-07-29 02:29:38 +0800

    fetch data from the server

---
+++ frontend/src/store.js
@@ -0,0 +1,12 @@
+import {createStore,combineReducers, applyMiddleware} from 'redux';
+import {productListReducer} from './reducers/productReducer'
+import thunk from 'redux-thunk';
+
+const initialState ={};
+const reducer = combineReducers({
+    productList:ProductListReducer,
+})
+
+const store = createStore(reducer,initialState,compose(applyMiddleware(thunk)));
+
+export default store;
